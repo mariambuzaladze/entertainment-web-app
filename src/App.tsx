1ee47335fcfdeb4748de103292328aca1a0c5539
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-03 00:53:12 +0400

    feat: add pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Movies from "./pages/Movies/Movies";
+import TVSeries from "./pages/TVSeries/TVSeries";
+import Bookmarked from "./pages/Bookmarked/Bookmarked";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/TVSeries" element={<TVSeries />} />
+        <Route path="/Bookmarked" element={<Bookmarked />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
