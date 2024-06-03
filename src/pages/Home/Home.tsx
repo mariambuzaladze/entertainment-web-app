8992af74ac092c38ce4b17a79239cbb021b162d6
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-03 18:45:55 +0400

    style: style trending section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,32 +12,38 @@ export default function Home() {
   const trending = data.data?.filter((e) => e.isTrending === true);
 
   return (
-    <div className="px-4 py-6">
+    <div className="px-4 py-6 flex flex-col gap-6">
       <SearchBar placeholder="Search for movies or TV series" />
-      <div>
-        <p>Trending</p>
-        <div>
+      <div className="flex flex-col gap-4">
+        <p className="text-white text-lg">Trending</p>
+        <div className="overflow-x-auto whitespace-nowrap flex gap-4">
           {trending?.map((e) => (
-            <div key={e.title}>
-              <div>
+            <div
+              key={e.title}
+              className="flex flex-col gap-10 rounded-md p-4"
+              style={{
+                backgroundImage: `url(${e.thumbnail.trending?.small})`,
+                backgroundSize: "cover",
+              }}
+            >
+              <div className="rounded-full bg-gray-800 p-2 w-fit self-end">
                 <img
                   src={e.isBookmarked ? BookmarkLogoFull : BookmarkLogoEmpty}
                   alt="Bookmarked icon"
                 />
               </div>
               <div>
-                <div>
+                <div className="flex items-center gap-3 text-sm text-white opacity-75 mr-[90px]">
                   <p>{e.year}</p>
-                  <div></div>
                   <img
                     src={e.category === "Movie" ? MoviesIcon : TVSeriesIcon}
                     alt="category icon"
                   />
                   <p>{e.category}</p>
-                  <div></div>
                   <p>{e.rating}</p>
                 </div>
-                <p>{e.title}</p>
+
+                <p className="text-white">{e.title}</p>
               </div>
             </div>
           ))}
