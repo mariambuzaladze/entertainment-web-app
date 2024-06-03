44ef043cee30c3420b11b3833731fa7efe01f1b4
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-04 01:55:00 +0400

    feat: add bookmarked page

--- src/pages/Bookmarked/Bookmarked.tsx
+++ src/pages/Bookmarked/Bookmarked.tsx
@@ -1,3 +1,55 @@
-export default function Bookmarked() {
-  return <></>;
+import { useContext } from "react";
+import { DataContext } from "../../App";
+import SearchBar from "../../components/SearchBar";
+import Item from "../../components/Item";
+
+interface IData {
+  title: string;
+  thumbnail: {
+    trending?: {
+      small: string;
+      large: string;
+    };
+    regular: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+  };
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
+}
+
+export default function TVSeries() {
+  const data = useContext(DataContext);
+  const movies = data.data?.filter((e) => e.isBookmarked);
+
+  const handleBookmarkClick = (clickedItem: IData) => {
+    data.setData((prevData) => {
+      return prevData.map((item) => {
+        if (item.title === clickedItem.title) {
+          return { ...item, isBookmarked: !item.isBookmarked };
+        }
+        return item;
+      });
+    });
+  };
+
+  return (
+    <main className="px-4 py-6 flex flex-col gap-6">
+      <SearchBar placeholder="Search for movies" />
+      <div className="flex flex-col gap-4">
+        <p className="text-white text-lg">Bookmarked Movies</p>
+
+        <div className="grid grid-cols-2 gap-4">
+          {movies?.map((e) => (
+            <Item key={e.title} e={e} onClick={() => handleBookmarkClick(e)} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
