a69f69f2569aa95e360f309f29ddb86353109ef9
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-04 00:18:07 +0400

    feat: add recommended section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -45,6 +45,7 @@ export default function Home() {
   return (
     <div className="px-4 py-6 flex flex-col gap-6">
       <SearchBar placeholder="Search for movies or TV series" />
+
       <div className="flex flex-col gap-4">
         <p className="text-white text-lg">Trending</p>
         <div className="overflow-x-auto whitespace-nowrap flex gap-4">
@@ -82,6 +83,44 @@ export default function Home() {
             </div>
           ))}
         </div>
+      </div>
+
+      <div>
+        {data.data?.map((e) => (
+          <div key={e.title}>
+            <div
+              className="flex flex-col gap-10 rounded-md p-4"
+              style={{
+                backgroundImage: `url(${e.thumbnail.regular.small})`,
+                backgroundSize: "cover",
+              }}
+            >
+              <div
+                className="rounded-full bg-gray-800 p-2 w-fit self-end"
+                onClick={() => handleBookmarkClick(e)}
+              >
+                <img
+                  src={e.isBookmarked ? BookmarkLogoFull : BookmarkLogoEmpty}
+                  alt="Bookmarked icon"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center gap-3 text-sm text-white opacity-75 mr-[90px]">
+                <p>{e.year}</p>
+                <img
+                  src={e.category === "Movie" ? MoviesIcon : TVSeriesIcon}
+                  alt="category icon"
+                />
+                <p>{e.category}</p>
+                <p>{e.rating}</p>
+              </div>
+
+              <p className="text-white">{e.title}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
