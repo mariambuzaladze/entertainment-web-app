3fed053c7fe6b0ac55f7b5305bf6a0665ff47787
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-04 23:53:14 +0400

    fix: fix styles of icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,10 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "/assets/logo.svg";
 import Temporary from "/vite.svg";
-import HomeIcon from "/assets/icon-nav-home.svg";
-import HomeIconPicked from "/assets/icon-nav-home.svg";
 import MoviesIcon from "/assets/icon-nav-movies.svg";
 import MoviesIconPicked from "/assets/icon-category-movie.svg";
 import TVSeriesIcon from "/assets/icon-nav-tv-series.svg";
 import TVSeriesIconPicked from "/assets/icon-category-tv.svg";
-import BookmarkedIcon from "/assets/icon-nav-bookmark.svg";
-import BookmarkedIconPicked from "/assets/icon-nav-bookmark.svg";
 
 export default function Header() {
   const [picked, setPicked] = useState("home");
@@ -19,11 +15,17 @@ export default function Header() {
       <img src={Logo} alt="icon logo" />
       <div className="flex gap-6">
         <Link to="/">
-          <img
-            src={picked === "home" ? HomeIconPicked : HomeIcon}
-            alt="Home page icon"
+          <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
             onClick={() => setPicked("home")}
-          />
+          >
+            <path
+              d="M8 0H1C.4 0 0 .4 0 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11H1c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1ZM19 0h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1Z"
+              fill={picked === "home" ? "#fff" : "#5A698F"}
+            />
+          </svg>
         </Link>
 
         <Link to="/movies">
@@ -45,13 +47,17 @@ export default function Header() {
         </Link>
 
         <Link to="/Bookmarked">
-          <img
-            src={
-              picked === "bookmarked" ? BookmarkedIconPicked : BookmarkedIcon
-            }
-            alt="Bookmarked icon"
+          <svg
+            width="17"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
             onClick={() => setPicked("bookmarked")}
-          />
+          >
+            <path
+              d="M15.387 0c.202 0 .396.04.581.119.291.115.522.295.694.542.172.247.258.52.258.82v17.038c0 .3-.086.573-.258.82a1.49 1.49 0 0 1-.694.542 1.49 1.49 0 0 1-.581.106c-.423 0-.79-.141-1.098-.423L8.46 13.959l-5.83 5.605c-.317.29-.682.436-1.097.436-.202 0-.396-.04-.581-.119a1.49 1.49 0 0 1-.694-.542A1.402 1.402 0 0 1 0 18.52V1.481c0-.3.086-.573.258-.82A1.49 1.49 0 0 1 .952.119C1.137.039 1.33 0 1.533 0h13.854Z"
+              fill={picked === "bookmarked" ? "#fff" : "#5A698F"}
+            />
+          </svg>
         </Link>
       </div>
       <img src={Temporary} alt="" />
